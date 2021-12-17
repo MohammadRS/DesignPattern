19453a9378f3b3f8477caf74e5409323604f4212
MohammadReza Sheykh Mohammadi <@m.mohammadi>
2021-12-17 14:25:09 +0330

    Proxy Implement

---
+++ 04_Proxy/Program.cs
@@ -0,0 +1,37 @@
+﻿ISubject subject = new Proxy();
+subject.DoAction();
+
+Console.WriteLine();
+
+public interface ISubject
+{
+    void DoAction();
+}
+
+public class RealSubject : ISubject
+{
+    public void DoAction()
+    {
+        Console.WriteLine("RealSubject.DoAction() is Run....");
+    }
+}
+
+public class Proxy : ISubject
+{
+    private RealSubject _realSubject;
+    public void DoAction()
+    {
+        GetRealSubject().DoAction();
+    }
+
+    protected RealSubject GetRealSubject()
+    {
+        if(_realSubject == null)
+        {
+            _realSubject = new RealSubject();
+        }
+        return _realSubject;
+    }
+}
+
+
