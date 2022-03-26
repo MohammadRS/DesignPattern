6d45e0625da802f19bfdc739414b2beb1ac96cf0
MohammadReza Sheykh Mohammadi <m.mohammadi@dinaweb.ir>
2022-03-26 15:01:57 +0430

    Singleton Pattern

---
+++ 09_Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿
+var obj1 = Singleton.GetInstance();
+var obj2 = Singleton.GetInstance();
+Console.WriteLine(obj1 == obj2);
+Console.ReadLine();
+
+public class Singleton
+{
+    private static readonly Singleton UnicInstance = new Singleton();
+
+    private Singleton()
+    {
+
+    }
+
+    public static Singleton GetInstance() => UnicInstance;
+}
